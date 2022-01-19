7bd483464ee66cca50c1c72e50505cce32390436
juansie96 <juansie.96@gmail.com>
2022-01-19 13:06:46 -0300

    second commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>naniiiiii</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laudantium recusandae nihil ratione veniam amet optio error rerum autem velit ut, dolores fugiat consequuntur temporibus sint, beatae commodi voluptatum sapiente.</p>
     </div>
   );
 }
