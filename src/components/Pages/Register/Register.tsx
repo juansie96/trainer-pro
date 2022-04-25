d818c83fb11e7f9336f3c1f774586f5d2a7e3cea
juansie96 <juansie.96@gmail.com>
2022-04-25 12:58:40 -0300

    Refactor register functionality

--- src/components/Pages/Register/Register.tsx
+++ src/components/Pages/Register/Register.tsx
@@ -1,49 +1,37 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
-import { Box, Button, LinearProgress } from "@mui/material";
-import { Controller, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-import { registerUserFB } from "./RegisterAPI";
-import { Input } from "../../Form/Input";
+import { Button, LinearProgress } from "@mui/material";
+import { useForm } from "react-hook-form";
 import { MainContainer } from "../../MainContainer/MainContainer";
-import { FormErrors } from "../../UI/Errors";
 import { CustomSnackbar } from "../../UI/CustomSnackbar";
-
-import { useAppDispatch, useAppSelector } from "../../../state/storeHooks";
-import { loadUserIntoApp, selectLoggedInUser } from "../../App/App.slice";
-import { validEmailRegex, validPwRegex } from "../../../constants/constants";
+import { useAppDispatch } from "../../../state/storeHooks";
+import { loadUserIntoApp } from "../../App/App.slice";
 import { mapFirebaseErrorCodeToMsg } from "../../../firebase/helperFunctions";
+import FormContainer from "../../Form/FormContainer";
+import { UserContext } from "../../../contexts/UserContext";
+import TextFieldElement from "../../Form/TextFieldElement";
+import { AuthError, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../firebase/firebase";
 
 type RegisterFormValues = {
-  Email: string;
-  Contraseña: string;
+  email: string;
+  password: string;
 };
 
 export const Register = () => {
-  const user = useAppSelector(selectLoggedInUser);
-  const dispatch = useAppDispatch();
+
+  const user = useContext(UserContext);
+
+  const formContext = useForm<RegisterFormValues>({
+    mode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   const [signingUp, setSigningUp] = useState(false);
   const [registerError, setRegisterError] = useState<any>("");
-
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<RegisterFormValues>({
-    mode: "onBlur",
-    defaultValues: {
-      Email: "",
-      Contraseña: "",
-    },
-    resolver: yupResolver(schema),
-  });
-
-  const formErrors: string[] = Object.values(errors)
-    .filter((e) => typeof e !== "undefined")
-    .map((e) => e!.message!);
 
   if (user) {
     return <Navigate to="/dashboard" />;
@@ -51,23 +39,25 @@ export const Register = () => {
 
   return (
     <MainContainer maxWidth="xs">
-      <FormErrors errors={formErrors} />
+      {/* <FormErrors errors={formErrors} /> */}
       <>{signingUp && <LinearProgress sx={{ my: 3 }} />}</>
-      <form onSubmit={handleSubmit(onSignUp)}>
-        {/* <Box my={2}>
-          <Controller
-            control={control}
-            name="Email"
-            render={(control) => <Input control={control} type="email" />}
-          />
-        </Box>
-        <Box my={2}>
-          <Controller
-            control={control}
-            name="Contraseña"
-            render={(control) => <Input control={control} type="password" />}
-          />
-        </Box> */}
+      <FormContainer formContext={formContext} handleSubmit={formContext.handleSubmit(onSignUp)}>
+        <TextFieldElement
+          sx={{ mb: 2 }}
+          fullWidth
+          label="Email"
+          name="email"
+          type="email"
+          validation={{ required: "El email es requerido" }}
+        />
+        <TextFieldElement
+          sx={{ mb: 2 }}
+          fullWidth
+          label="Password"
+          name="password"
+          type="password"
+          validation={{ required: "La contraseña es requerida" }}
+        />
         <Button
           variant="contained"
           type="submit"
@@ -76,7 +66,7 @@ export const Register = () => {
         >
           {signingUp ? "Creando cuenta" : "Registrarse"}
         </Button>
-      </form>
+      </FormContainer>
 
       <CustomSnackbar
         open={!!registerError}
@@ -94,27 +84,15 @@ export const Register = () => {
   async function onSignUp(user: RegisterFormValues) {
     setSigningUp(true);
     try {
-      const response = await registerUserFB(user.Email, user.Contraseña);
+      await createUserWithEmailAndPassword(auth,user.email, user.password);
       setSigningUp(false);
       setRegisterError("");
-      dispatch(loadUserIntoApp(response));
-    } catch (err: any) {
+    } catch (err) {
+      const error = err as AuthError;
       setSigningUp(false);
-      setRegisterError(mapFirebaseErrorCodeToMsg(err.code));
+      setRegisterError(mapFirebaseErrorCodeToMsg(error.code));
+      formContext.setValue("password", "");
     }
   }
 };
 
-const schema = yup.object().shape({
-  Email: yup
-    .string()
-    .required("El email es requerido")
-    .matches(validEmailRegex, "El email ingresado no es correcto"),
-  Contraseña: yup
-    .string()
-    .required("La contraseña es requerida")
-    // .matches(
-    //   validPwRegex,
-    //   "La contraseña debe contener como mínimo una letra en mayúscula, otra en minúscula, un número y como mínimo debe tener 6 carácteres"
-    // ),
-});
